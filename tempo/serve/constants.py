fd2f6982856ba8521befd6ad5c17ca48d7d55718
Adrian Gonzalez-Martin <agm@seldon.io>
2021-02-12 17:33:47 +0000

    Update mlserver deps

--- tempo/serve/constants.py
+++ tempo/serve/constants.py
@@ -14,5 +14,4 @@ DefaultModelFilename = "model.pickle"
 DefaultEnvFilename = "environment.tar.gz"
 
 # TODO: Update once tempo is published
-#  MLServerEnvDeps = ["mlserver==0.3.1.dev0", "mlserver-tempo==0.3.1.dev0"]
-MLServerEnvDeps = ["mlserver==0.3.0.dev1"]
+MLServerEnvDeps = ["mlserver==0.3.1.dev1", "mlserver-tempo==0.3.1.dev1"]
