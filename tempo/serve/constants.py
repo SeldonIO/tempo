faf8a50708483d5aa60aaebcef325b513b5ea3b6
Adrian Gonzalez-Martin <agm@seldon.io>
2021-03-31 12:38:32 +0100

    Fix versions

--- tempo/serve/constants.py
+++ tempo/serve/constants.py
@@ -14,4 +14,4 @@ DefaultModelFilename = "model.pickle"
 DefaultEnvFilename = "environment.tar.gz"
 
 # TODO: Update once tempo is published
-MLServerEnvDeps = ["mlserver==0.3.1.dev6"]
+MLServerEnvDeps = ["mlserver==0.3.1.dev5"]
