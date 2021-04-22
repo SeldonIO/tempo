1c59081f1de3bf6fd7cad06cfd304a2f3309164a
Adrian Gonzalez-Martin <agm@seldon.io>
2021-04-22 13:17:50 +0200

    Add pytest-cases

--- tests/test_mlserver.py
+++ tests/test_mlserver.py
@@ -1,30 +1,26 @@
 import pytest
-from mlserver.settings import ModelParameters, ModelSettings
+
+from pytest_cases import fixture, parametrize_with_cases
+
+from mlserver.settings import ModelSettings
 from mlserver.types import InferenceRequest, RequestInput
 from mlserver.utils import to_ndarray
 
 from tempo import Model
 from tempo.mlserver import InferenceRuntime
-from tempo.serve.loader import save
-
-
-@pytest.fixture
-def model_settings(custom_model: Model) -> ModelSettings:
-    save(custom_model, save_env=False)
-    model_uri = custom_model.details.local_folder
-
-    return ModelSettings(
-        name="sum-model",
-        parameters=ModelParameters(uri=model_uri),
-    )
 
 
 @pytest.fixture
 def inference_request() -> InferenceRequest:
-    return InferenceRequest(inputs=[RequestInput(name="input-0", shape=[4], data=[1, 2, 3, 4], datatype="FP32")])
+    return InferenceRequest(
+        inputs=[
+            RequestInput(name="input-0", shape=[4], data=[1, 2, 3, 4], datatype="FP32")
+        ]
+    )
 
 
-@pytest.fixture
+@fixture
+@parametrize_with_cases("model_settings")
 async def mlserver_runtime(model_settings: ModelSettings) -> InferenceRuntime:
     _runtime = InferenceRuntime(model_settings)
     await _runtime.load()
@@ -47,7 +43,9 @@ async def test_predict(
     assert len(res.outputs) == 1
 
     pipeline_input = to_ndarray(inference_request.inputs[0])
-    custom_model.get_tempo().set_remote(False)  # ensure direct call to class does not try to do remote
+    custom_model.get_tempo().set_remote(
+        False
+    )  # ensure direct call to class does not try to do remote
     expected_output = custom_model(pipeline_input)
 
     pipeline_output = res.outputs[0].data
