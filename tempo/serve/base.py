98877bc2138156c808126dd9703f91edb6220f8f
Alejandro Saucedo <axsauze@gmail.com>
2021-04-22 14:30:38 +0100

    Rebased commit

--- tempo/serve/base.py
+++ tempo/serve/base.py
@@ -21,7 +21,6 @@ from .remote import Remote
 from .runtime import ModelSpec, Runtime
 from .types import LoadMethodSignature, ModelDataType, PredictMethodSignature
 from tempo.serve.state import BaseState, DistributedState, LocalState, StateDetails, StateType
-from tempo.utils import logger, tempo_settings
 
 
 class BaseModel:
