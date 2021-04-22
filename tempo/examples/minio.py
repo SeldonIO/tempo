4dd39a5d485223826edfae0208ddb3917fb05a2e
Alejandro Saucedo <axsauze@gmail.com>
2021-04-22 15:40:24 +0100

    Update minio.py

--- tempo/examples/minio.py
+++ tempo/examples/minio.py
@@ -17,11 +17,9 @@ def create_minio_rclone(path: str):
     config.load_kube_config()
     api_instance = client.CoreV1Api()
     res = api_instance.list_namespaced_service("minio-system", field_selector="metadata.name=minio")
-
     minio_ip = res.items[0].status.load_balancer.ingress[0].ip
     if not minio_ip:
         minio_ip = res.items[0].status.load_balancer.ingress[0].hostname
-    
     rclone_conf = RCloneConf.replace("{MINIO_IP}", minio_ip)
     with open(path, "w") as f:
         f.write(rclone_conf)
