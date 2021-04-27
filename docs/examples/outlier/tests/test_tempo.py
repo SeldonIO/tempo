86ecd32a823586dcfc6edbcb929509c55b09bd1d
Adrian Gonzalez-Martin <agm@seldon.io>
2021-04-27 10:24:44 +0200

    Updated tests and src to use ARTIFACTS_FOLDER constant

--- docs/examples/outlier/tests/test_tempo.py
+++ docs/examples/outlier/tests/test_tempo.py
@@ -3,10 +3,10 @@ from src.tempo import create_model, create_outlier_cls, create_svc_cls
 
 
 def test_svc_outlier():
-    model = create_model("")
-    OutlierModel = create_outlier_cls("")
+    model = create_model()
+    OutlierModel = create_outlier_cls()
     outlier = OutlierModel()
-    Cifar10Svc = create_svc_cls(outlier, model, "")
+    Cifar10Svc = create_svc_cls(outlier, model)
     svc = Cifar10Svc()
     svc.models.outlier = lambda payload: {"data": {"is_outlier": [1]}}
     svc.models.cifar10 = lambda input: np.array([[0.2]])
@@ -15,10 +15,10 @@ def test_svc_outlier():
 
 
 def test_svc_inlier():
-    model = create_model("")
-    OutlierModel = create_outlier_cls("")
+    model = create_model()
+    OutlierModel = create_outlier_cls()
     outlier = OutlierModel()
-    Cifar10Svc = create_svc_cls(outlier, model, "")
+    Cifar10Svc = create_svc_cls(outlier, model)
     svc = Cifar10Svc()
     svc.models.outlier = lambda payload: {"data": {"is_outlier": [0]}}
     svc.models.cifar10 = lambda input: np.array([[0.2]])
