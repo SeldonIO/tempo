889951aa3565854a7ee16fd4fa26c2f038f84811
Alejandro Saucedo <axsauze@gmail.com>
2021-06-09 22:04:40 +0100

    Added initial implementation of insights logger

--- tempo/insights/logger.py
+++ tempo/insights/logger.py
@@ -1,7 +1,8 @@
+import asyncio
 
 from .worker import start_insights_worker_from_async, start_insights_worker_from_sync
 
-class AsyncInsightsLogger:
+class InsightsLogger:
     def __init__(
             self,
             worker_endpoint: str = "",
@@ -11,7 +12,7 @@ class AsyncInsightsLogger:
             output_file_path: str = None,
             window_time: int = None,
         ):
-        self._q = start_insights_worker_from_sync(
+        args = (
             worker_endpoint,
             batch_size,
             parallelism,
@@ -19,31 +20,13 @@ class AsyncInsightsLogger:
             output_file_path,
             window_time,
         )
+        try:
+            asyncio.get_running_loop()
+        except:
+            self._q = start_insights_worker_from_sync(*args)
+        else:
+            self._q = start_insights_worker_from_async(*args)
 
     def log(self, data):
         self._q.put(data)
-
-
-class SyncInsightsLogger:
-    def __init__(
-            self,
-            worker_endpoint: str = "",
-            batch_size: int = 1,
-            parallelism: int = 1,
-            retries: int = 3,
-            output_file_path: str = None,
-            window_time: int = None,
-        ):
-        self._q = start_insights_worker_from_sync(
-            worker_endpoint,
-            batch_size,
-            parallelism,
-            retries,
-            output_file_path,
-            window_time,
-        )
-
-    def log(self, data):
-        self._q.put(data)
-
 
