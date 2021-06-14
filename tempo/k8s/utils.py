d2ff8e7c9f539c2cafd07dd16f409e60b2f08f35
Alejandro Saucedo <axsauze@gmail.com>
2021-06-14 13:32:52 +0100

    Updated k8s insights deployer

--- tempo/k8s/utils.py
+++ tempo/k8s/utils.py
@@ -32,58 +32,78 @@ def deploy_insights_message_dumper():
             DefaultInsightsServiceName,
             DefaultSeldonSystemNamespace,
         )
+        logger.debug(f"Pod with name {DefaultInsightsServiceName} in namespace {DefaultSeldonSystemNamespace} already exists")
     except ApiException as e:
         if e.status != 404:
             raise e
 
-    k8s_pod_spec = {
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": {
-            "name": DefaultInsightsServiceName,
-            "labels": {
-                "app": DefaultInsightsServiceName,
+        k8s_pod_spec = {
+            "apiVersion": "v1",
+            "kind": "Pod",
+            "metadata": {
+                "name": DefaultInsightsServiceName,
+                "labels": {
+                    "app": DefaultInsightsServiceName,
+                },
             },
-        },
-        "spec": {
-            "containers": [
-                {
-                    "name": "default",
-                    "image": DefaultInsightsImage,
-                    "ports": [{"containerPort": DefaultInsightsPort}],
-                }
-            ]
-        },
-    }
-    logger.debug(f"Creating kubernetes insights manager pod: {k8s_pod_spec}")
-    api_instance.create_namespaced_pod(DefaultSeldonSystemNamespace, k8s_pod_spec)
+            "spec": {
+                "containers": [
+                    {
+                        "name": "default",
+                        "image": DefaultInsightsImage,
+                        "ports": [{"containerPort": DefaultInsightsPort}],
+                    }
+                ]
+            },
+        }
+        logger.debug(f"Creating kubernetes insights manager pod: {k8s_pod_spec}")
+        api_instance.create_namespaced_pod(DefaultSeldonSystemNamespace, k8s_pod_spec)
 
-    k8s_svc_spec = {
-        "apiVersion": "v1",
-        "kind": "Service",
-        "metadata": {
-            "name": DefaultInsightsServiceName,
-        },
-        "spec": {
-            "selector": {"app": DefaultInsightsServiceName},
-            "ports": [
-                {
-                    "port": DefaultInsightsPort,
-                    "targetPort": DefaultInsightsPort,
-                }
-            ],
-        },
-    }
-    logger.debug(f"Creating kubernetes insights manager pod: {k8s_svc_spec}")
-    api_instance.create_namespaced_service(DefaultSeldonSystemNamespace, k8s_svc_spec)
+    try:
+        api_instance.read_namespaced_service(
+            DefaultInsightsServiceName,
+            DefaultSeldonSystemNamespace,
+        )
+        logger.debug(f"Service with name {DefaultInsightsServiceName} in namespace {DefaultSeldonSystemNamespace} already exists")
+    except ApiException as e:
+        if e.status != 404:
+            raise e
+
+        k8s_svc_spec = {
+            "apiVersion": "v1",
+            "kind": "Service",
+            "metadata": {
+                "name": DefaultInsightsServiceName,
+            },
+            "spec": {
+                "selector": {"app": DefaultInsightsServiceName},
+                "ports": [
+                    {
+                        "port": DefaultInsightsPort,
+                        "targetPort": DefaultInsightsPort,
+                    }
+                ],
+            },
+        }
+
+        logger.debug(f"Creating kubernetes insights manager pod: {k8s_svc_spec}")
+        api_instance.create_namespaced_service(DefaultSeldonSystemNamespace, k8s_svc_spec)
 
 
 def undeploy_insights_message_dumper():
     create_k8s_client()
     api_instance = client.CoreV1Api()
-    api_instance.delete_namespaced_pod(DefaultInsightsServiceName, DefaultSeldonSystemNamespace)
-    api_instance.delete_namespaced_service(DefaultInsightsServiceName, DefaultSeldonSystemNamespace)
+    try:
+        api_instance.delete_namespaced_pod(DefaultInsightsServiceName, DefaultSeldonSystemNamespace)
+    except ApiException as e:
+        if e.status != 404:
+            raise e
 
+    try:
+        api_instance.delete_namespaced_service(DefaultInsightsServiceName, DefaultSeldonSystemNamespace)
+    except ApiException as e:
+        if e.status != 404:
+            raise e
 
 def get_logs_insights_message_dumper():
     create_k8s_client()
