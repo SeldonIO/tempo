03352654055397c702170c42c2af50893d9e079f
Adrian Gonzalez-Martin <agm@seldon.io>
2021-08-05 16:18:42 +0200

    Fix linter

--- tempo/serve/constants.py
+++ tempo/serve/constants.py
@@ -23,9 +23,7 @@ DefaultInsightsImage = "mendhak/http-https-echo:18"
 DefaultInsightsLocalEndpoint = "http://0.0.0.0:8080"
 DefaultInsightsDockerEndpoint = f"http://{DefaultInsightsServiceName}:8080"
 DefaultSeldonSystemNamespace = "seldon-system"
-DefaultInsightsK8sEndpoint = (
-    f"http://{DefaultInsightsServiceName}.{DefaultSeldonSystemNamespace}:8080"
-)
+DefaultInsightsK8sEndpoint = f"http://{DefaultInsightsServiceName}.{DefaultSeldonSystemNamespace}:8080"
 
 
 DefaultRedisServiceName = "redis-master"
